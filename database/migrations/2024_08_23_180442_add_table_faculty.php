b2cd9761e7278334f641c8e6164617e4cfcf5c8c
Deineee <deinepomperad33@gmail.com>
2024-08-24 02:11:46 +0800

    added migration file for adding faculty table in the database

---
+++ database/migrations/2024_08_23_180442_add_table_faculty.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faculty', function (Blueprint $table) {
+            $table->id();
+            
+            // Reference to the teacher's name and email from the users table
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name')->nullable(); 
+            $table->string('full_name')->nullable();
+            $table->string('email')->nullable(); 
+            $table->string('status')->nullable();
+
+            // Reference to the semester
+            $table->foreignId('semester_id')->constrained('semester');
+            $table->string('semester_name')->nullable(); 
+            
+            // Reference to the department
+            $table->foreignId('department_id')->constrained('department');
+            $table->string('department_name')->nullable(); 
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faculty');
+    }
+};
